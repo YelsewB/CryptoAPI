a03a491e147f0625d5ae171567086fbd64fc4442
Wesley Blom <wesley.blom@serviceheroes.com>
2019-02-15 12:48:02 +0100

    Cleanup

--- target/classes/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO Currency (ticker, name, number_of_coins, market_cap) VALUES
-            ('BTC', 'Bitcoin', '16770000', '189580000000' ),
-            ('ETH', 'Ethereum', '96710000', '69280000000' ),
-            ('XRP', 'Ripple', '38590000000', '64750000000' ),
-            ('BCH', 'BitcoinCash', '16670000', '69020000000' );
